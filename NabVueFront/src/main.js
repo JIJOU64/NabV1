a3ef39c2729e06147995ceb6d1be8470316e4948
JIJOU64 <jerometuron@gmail.com>
2025-01-25 18:10:41 +0100

    Adding Vuetify and modifying configuration files

--- NabVueFront/src/main.js
+++ NabVueFront/src/main.js
@@ -1,14 +1,26 @@
 import './assets/main.css'
 
+// Vuetify import
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+// Initializing Vuetify
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
 
-app.mount('#app')
+createApp(App)
+    .use(vuetify)      // Apply Vuetify
+    .use(createPinia()) // Apply Pinia
+    .use(router)        // Apply the router
+    .mount('#app')      // Mount application in element with the ID 'app'
