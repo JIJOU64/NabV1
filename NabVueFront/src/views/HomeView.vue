6228cf46be0b8ac0a0f49b10565c9a69ddf15470
JIJOU64 <jerometuron@gmail.com>
2025-01-27 10:26:59 +0100

    WIP App.vue

--- NabVueFront/src/views/HomeView.vue
+++ NabVueFront/src/views/HomeView.vue
@@ -1,5 +1,23 @@
 <template>
   <v-main app fluid>
+    <!-- Ajout de la classe d-flex pour utiliser flexbox, justify-center pour centrer horizontalement, et align-center pour centrer verticalement -->
+    <v-container fluid class="d-flex justify-center align-center" style="min-height: 100vh;">
+      <v-card>
+        <v-card-title class="text-h4 white--text text-center">
+          Votre Univers Pro, Votre gestion Intuitive
+        </v-card-title>
+        <v-card-subtitle class="text-h6 white--text text-center">
+          Application de Bureau et Mobile Personnalisée
+        </v-card-subtitle>
+      </v-card>
+    </v-container>
+  </v-main>
+</template>
+
+
+<!--
+<template>
+  <v-main app fluid>
     <div
       class="d-flex justify-center align-center"
       style="height: 100vh; width: 100%;"
@@ -16,7 +34,7 @@
 
 
 
-<!--<template>
+<template>
   <v-main app
     fluid
     class="d-flex align-center justify-center"
@@ -41,28 +59,7 @@
 </template>
 
 
-<template>
-  <v-main app fluid>
-
-    <v-card
-      class="d-flex justify-center align-center"
-      :style="{
-        backgroundImage: 'url(' + imageUrl + ')',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        height: '100vh',
-        width: '100%'  // S'assure que la carte prend toute la largeur
-      }"
-    >
-      <v-card-title class="text-h4 white--text text-center">
-        Votre Univers Pro, Votre gestion Intuitive
-      </v-card-title>
-      <v-card-subtitle class="text-h6 text-white text-center">
-        Application de Bureau et Mobile Personnalisée
-      </v-card-subtitle>
-    </v-card>
-  </v-main>
-</template>-->
+-->
 
 <script setup>
 // L'URL de l'image
