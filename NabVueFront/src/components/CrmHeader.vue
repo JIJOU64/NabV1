c1379bbffe91181633479d645cf2eebc91e87e93
Jerome <jerometuron@gmail.com>
2025-02-06 09:37:18 +0100

    Reinstalling tailwind?? and Update CrmHeader with tailwind

--- NabVueFront/src/components/CrmHeader.vue
+++ NabVueFront/src/components/CrmHeader.vue
@@ -1,17 +1,22 @@
 <template>
-  <header v-bind="$attrs" class="p-4 flex items-center justify-between bg-blue-500 text-white">
-    <div class="flex items-center space-x-2">
-      <HomeIcon class="w-6 h-6 text-white" />
-      <UserCircleIcon class="w-6 h-6 text-white" />
-      
-
-      <span>Accueil</span>
-    </div>
-    
+  <header 
+    v-bind="$attrs"
+    :style="imageUrlPhoto ? { backgroundImage: `url(${imageUrlPhoto})` } : {}" 
+    class="p-4 flex items-center justify-between bg-blue-500 text-white bg-cover bg-center">
     <!-- Images du logo -->
     <div class="flex items-center space-x-4">
       <img v-if="imageUrlLogo" :src="imageUrlLogo" alt="Logo" class="h-10">
-      <img v-if="imageUrlPhoto" :src="imageUrlPhoto" alt="Image de Fond" class="h-10">
+    </div>
+    <div class="flex items-center space-x-2">
+      <HomeIcon class="w-6 h-6 text-white" />
+      <span>Accueil</span>
+      <UserIcon class="w-6 h-6 text-white" />
+      <span>Clients</span>
+      <ChartBarIcon class="w-6 h-6 text-white" />
+      <span>Bilans</span>
+      <DocumentIcon class="w-6 h-6 text-white" />
+      <span>Documents</span>
+      
     </div>
   </header>
 </template>
@@ -20,7 +25,7 @@
 console.log("Liaison page?")
 
 // Import des icônes Heroicons
-import { HomeIcon } from '@heroicons/vue/24/outline'
+import { HomeIcon, UserIcon, ChartBarIcon, DocumentIcon } from '@heroicons/vue/24/outline'
 
 // Déclaration des images
 const imageUrlLogo = new URL('@/assets/images/LogoNabiha.png', import.meta.url).href;
