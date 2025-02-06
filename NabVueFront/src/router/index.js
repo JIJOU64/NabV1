c05bfd43ba41918dab195da0fd581cc4a18fec27
Jerome <jerometuron@gmail.com>
2025-02-06 10:53:44 +0100

    Update index.js, Add ClientsFilter and delete then ignore Brouillon.vue

--- NabVueFront/src/router/index.js
+++ NabVueFront/src/router/index.js
@@ -10,6 +10,11 @@ const router = createRouter({
       component: HomeView,
     },
     {
+      path: '/client',
+      name: 'client',
+      component: () => import('../views/Clients/ClientsFilter.vue')
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
---
+++ NabVueFront/src/views/Clients/ClientsFilter.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="client">
+      <section class="py-12">
+        <div class="container mx-auto px-4">
+  
+          <!-- Titre principal -->
+          <h1 class="text-4xl font-bold text-center mb-4">Menu Client</h1>
+          <h2 class="text-xl text-center mb-8 text-gray-600">Accès aux clients</h2>
+
+          
+        </div>
+      </section>
+    </div>
+</template>
+  
