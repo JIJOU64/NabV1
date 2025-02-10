76049d05986623bbb78b6e002178788b26167d8f
Jerome <jerometuron@gmail.com>
2025-02-10 10:08:05 +0100

    Update index.js to include ClientChoice.vue

--- NabVueFront/src/router/index.js
+++ NabVueFront/src/router/index.js
@@ -15,6 +15,11 @@ const router = createRouter({
       component: () => import('../views/Clients/ClientsFilter.vue')
     },
     {
+      path: '/client/choice',
+      name: 'clientchoice',
+      component: () => import('../views/Clients/ClientChoice.vue')
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
