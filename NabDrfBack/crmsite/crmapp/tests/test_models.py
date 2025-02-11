e49ac3f761b5ea841e9cdf97373699deb05b5c77
Jerome <jerometuron@gmail.com>
2025-02-11 09:49:45 +0100

    added pytest to requirements.txt

---
+++ NabDrfBack/crmsite/crmapp/tests/test_models.py
@@ -0,0 +1,22 @@
+import pytest
+from .models import ClientProgress, ClientsData
+
+@pytest.mark.django_db
+def test_create_client_progress():
+    client_progress = ClientProgress.objects.create(name_progress="Completed")
+    assert client_progress.id_progress is not None
+    assert client_progress.name_progress == "Completed"
+
+
+@pytest.mark.django_db
+def test_create_clients_data():
+    client_progress = ClientProgress.objects.create(name_progress="In Progress")
+
+    clients_data = ClientsData.objects.create(
+        file_creation_date="2025-01-01",
+        file_closing_date="2025-02-01",
+        id_progress_client_progress=client_progress
+    )
+
+    assert clients_data.id_client is not None
+    assert clients_data.id_progress_client_progress == client_progress
